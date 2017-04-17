549c509291512ccca31ad935be04c48c6629471f
Oihan <oihan@MacBook-Pro-de-Oihan.local>
2017-04-17 17:29:08 +0200

    Login garatu

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-	root "index#index"
+	get 'sessions/login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
