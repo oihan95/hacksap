ebb1deae766c75ef983bf6276a0ffbf896545ee6
Oihan <oihan@MacBook-Pro-de-Oihan.local>
2017-04-18 00:37:15 +0200

    Erregistratzeko formularioa sortuta
    Funtzionalitatea ez dago inplementatuta, ezta bi orrien arteko
    kontrolak ere ez

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-	root "index#index"
+	root :to => "index#index", :id => '1'
+	get 'users/new'
+	resources :users
 	get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
