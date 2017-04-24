5f641599ee39b1a8a5c38ece208d345f1bd88a44
Oihan <oihan@MacBook-Pro-de-Oihan.local>
2017-04-24 19:36:45 +0200

    Aldaketa txiki batzuk

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	root :to => "index#index", :id => '1'
-	get 'users/new'
-	resources :users
 	get  '/signup',  to: 'users#new'
+	post '/signup',  to: 'users#create'
+	resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
