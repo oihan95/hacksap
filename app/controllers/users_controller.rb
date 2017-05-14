419efd748ae41137d652a676f28d23119f6c882c
Oihan <oihan@MacBook-Pro-de-Oihan.local>
2017-05-15 00:47:21 +0200

    Erregistratu funtzionalitatea garatuta

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    	redirect_to @user
+      flash[:success] = "Erabiltzailea ongi sortu da"
+      redirect_to :back      
     else
       render 'new'
     end
