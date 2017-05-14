9587bbd6aef28b95e955b0833637a84daa367b9c
Oihan <oihan@MacBook-Pro-de-Oihan.local>
2017-05-14 09:27:00 +0200

    6.2.1-en aldaketak eta 6.2.2 eginda

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+	validates :izena, presence: true
+	validates :erab_izena, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,10 +2,20 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(izena: "Example User", erab_izena: "user@example.com")
   end
 
   test "should be valid" do
     assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.izena = "     "
+    assert_not @user.valid?
+  end
+
+  test "erab_izena should be present" do
+    @user.erab_izena = "     "
+    assert_not @user.valid?
   end
 end
