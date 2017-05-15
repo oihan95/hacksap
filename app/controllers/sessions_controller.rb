bb3a3328620e858f07fbba3d9a2b72d89caf4c94
Oihan <oihan@MacBook-Pro-de-Oihan.local>
2017-05-15 23:40:34 +0200

    Login funtzionalitatea garatuta
    Arazo bakarra: Login egiterakoan, errore bat badago (erabiltzailea ez
    da existitzen edo pasahitza ez da egokia), erabiltzailearen kaxan
    zegoen informazioa ezabatu egiten da

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    erab = params[:session][:erab_izena].downcase
     user = User.find_by(erab_izena: params[:session][:erab_izena].downcase)
-    if user && user.authenticate(params[:session][:password])
-    	log_in user
-    	redirect_to menu_path
-    else
+    if user.nil?
+      flash.now[:danger] = 'Erabiltzaile hau ez da existitzen'
       render 'new'
+    else
+      if user.authenticate(params[:session][:password])
+        log_in user
+        redirect_to menu_path
+      else
+        flash.now[:danger] = 'Pasahitza ez da zuzena'
+        render 'new'
+      end
     end
   end
 
