a22e0511a9fac75d7e47060e6f566b06e3b3820b
Oihan <oihan@MacBook-Pro-de-Oihan.local>
2017-05-15 09:32:18 +0200

    8.1.1 ariketa eindde

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 	root :to => "index#index", :id => '1'
 	get  '/signup',  to: 'users#new'
 	post '/signup',  to: 'users#create'
+	get    '/login',   to: 'sessions#new'
+  	post   '/login',   to: 'sessions#create'
+  	delete '/logout',  to: 'sessions#destroy'
 	resources :users
 end
