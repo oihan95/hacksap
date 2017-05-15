5ba82ab196638d88e1103392ed91709a8a2ec532
Oihan <oihan@MacBook-Pro-de-Oihan.local>
2017-05-15 09:38:41 +0200

    8.1.3 ariketa eindde

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
-  def login
+  def new
+  end
+
+  def create
+    user = User.find_by(erab_izena: params[:session][:erab_izena].downcase)
+    if user && user.authenticate(params[:session][:pasahitza])
+      # Log the user in and redirect to the user's show page.
+    else
+      # Create an error message.
+      render 'new'
+    end
+  end
+
+  def destroy
   end
 end
