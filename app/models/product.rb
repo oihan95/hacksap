51fa6e52471acaa6915da4258c2e190ad12ed4f7
landa95 <95landa@gmail.com>
2017-05-16 18:07:20 +0200

    gauza txikiak

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
 
-  validates :material, presence:true, uniqueness: {case_sensitive: true}
-  validates :text, presence:true
+  validates :material, presence: true, uniqueness: {case_sensitive: false}
+  validates :text, presence: true
   validates :reference, presence: true, length: {maximum:20 }
   validates :quantity, presence: true
 end
