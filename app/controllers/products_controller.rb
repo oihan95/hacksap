78633ad0f53b46895de52e80c07f5fb6e98c5ceb
Oihan <oihan@MacBook-Pro-de-Oihan.local>
2017-05-17 14:18:39 +0200

    Ein duazen aldaketak
    Produktuek ez die bistaratzen

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController< ApplicationController
   def new
     @product = Product.new
   end
+
   def create
     @product = Product.new(products_param)
     if @product.save
@@ -16,9 +17,13 @@ class ProductsController< ApplicationController
   def menu
     render 'menu/product'
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
   private
   def products_param
     params.require(:product).permit(:material,:text, :reference, :quantity)
-
   end
 end
